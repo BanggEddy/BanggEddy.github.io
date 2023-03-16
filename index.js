9989e01cf19c30eebdda27d61fbb24c05b59507e
David LI <108392457+BanggEddy@users.noreply.github.com>
2023-03-16 18:30:22 +0100

    intégrée js color storage

--- index.js
+++ index.js
@@ -29,37 +29,66 @@ theme.forEach((item) => {
   });
 });
 
-('localStorage.getItem("themeColor");');
-('localStorage.setItem("themeColor", "dark") ;');
+// récupération de la couleur du thème dans le localStorage
+const savedTheme = localStorage.getItem("themeColor");
+if (savedTheme) {
+  document.body.classList.add(savedTheme);
+}
 
-const OuvrirFermer = document.querySelector("#OuvrirFermer"); //ID=ouvrir
-const img = document.getElementById("img"); //ID=img
+theme.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    document.body.classList.remove("darkTheme");
+    document.body.classList.remove("yellowTheme");
+    document.body.classList.remove("salmonTheme");
+    document.body.classList.remove("whiteTheme");
 
+    switch (e.target.id) {
+      case "dark":
+        document.body.classList.add("darkTheme");
+        // stockage de la couleur du thème sélectionné dans le localStorage
+        localStorage.setItem("themeColor", "darkTheme");
+        break;
+      case "salmon":
+        document.body.classList.add("salmonTheme");
+        localStorage.setItem("themeColor", "salmonTheme");
+        break;
+      case "yellow":
+        document.body.classList.add("yellowTheme");
+        localStorage.setItem("themeColor", "yellowTheme");
+        break;
+      case "white":
+        document.body.classList.add("whiteTheme");
+        localStorage.setItem("themeColor", "whiteTheme");
+        break;
+      default:
+        null;
+    }
+  });
+});
 
 //Send Email Form JS
-function sendMail(){
+function sendMail() {
   var params = {
-    name: document.getElementById("name").value,    name: document.getElementById("name").value,
+    name: document.getElementById("name").value,
     prenom: document.getElementById("prenom").value,
     email: document.getElementById("email").value,
     phone: document.getElementById("phone").value,
     message: document.getElementById("message").value,
   };
 
-const serviceID ="service_5v104qn";
-const templateID="template_w7162pr";
+  const serviceID = "service_5v104qn";
+  const templateID = "template_w7162pr";
 
-emailjs
-.send(serviceID, templateID, params) 
-.then((res)=>{
-    document.getElementById("name").value ="";
-    document.getElementById("prenom").value ="";
-    document.getElementById("email").value ="";
-    document.getElementById("phone").value ="";
-    document.getElementById("message").value ="";
-    console.log(res);
-    alert("your message sent successfully");
-})
-.catch((err) => console.log(err));
-
+  emailjs
+    .send(serviceID, templateID, params)
+    .then((res) => {
+      document.getElementById("name").value = "";
+      document.getElementById("prenom").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("phone").value = "";
+      document.getElementById("message").value = "";
+      console.log(res);
+      alert("your message sent successfully");
+    })
+    .catch((err) => console.log(err));
 }
